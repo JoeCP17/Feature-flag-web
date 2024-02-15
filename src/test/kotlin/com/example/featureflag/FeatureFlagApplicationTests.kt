4e0a828d15a80adef0b15157e551000350892ab1
Ueibin Kim <at365@naver.com>
2024-02-15 23:05:54 +0900

    setting : Setting Projects

---
+++ src/test/kotlin/com/example/featureflag/FeatureFlagApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.featureflag
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeatureFlagApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
